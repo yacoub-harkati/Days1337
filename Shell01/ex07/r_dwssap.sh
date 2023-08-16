8a85e34acae9dad9a965a2a53b973c3353287603
yaharkat <yaharkat@e2r8p2.1337.ma>
2023-08-16 01:31:59 +0100

    Shell01

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | sed -n '$FT_LINE1,$FT_LINE2  p' | tr '\n' ' ' | sed 's/ /, /g' | sed 's/, $/./'
+cat /etc/passwd | grep -v "^#" | awk "NR%2==0" | awk -F: '{print $1}' | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ' ' | sed 's/ /, /g' | sed 's/, $/./' | xargs -I{} echo -n {}
