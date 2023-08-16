33ddf7a828c1ccaa0ee95885a02c004faa61c904
yaharkat <yaharkat@e2r2p5.1337.ma>
2023-08-16 12:17:31 +0100

    jbj

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */#include <unistd.h>
+
+
+void ft_putchar(char c){
+	write(1,&c, 1);
+}
+
+
+void ft_print_alphabet(void){
+	char a = 'a';
+	while(a<='z'){
+		ft_putchar(a++);
+	}
+}
+
+
+int main(void){
+	ft_print_alphabet();
+	return 0;
+}
