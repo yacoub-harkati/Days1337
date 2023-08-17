b6dbb9433373a7ca8a8ec69508932e147914f8c2
yaharkat <yaharkat@e2r3p5.1337.ma>
2023-08-17 22:45:07 +0100

    test

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: yaharkat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 19:08:25 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/08/17 20:43:37 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/08/17 22:44:21 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,29 @@ void	ft_swap(int *a, int *b)
 	*b = tmp;
 }
 
-void ft_sort_int_tab(int *tab, int size)
-{   
-    int index;
-    
-    index = 0;
-    while (index < size)
-    {
-        
-    }
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	index;
+	int	j;
+	int	tmp;
+	int	is_swapped;
+
+	index = 0;
+	is_swapped = 0;
+	while (index < size)
+	{
+		j = 0;
+		while (j < size - index - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+				is_swapped = 1;
+			}
+			j++;
+		}
+		if (!is_swapped)
+			break ;
+		index++;
+	}
 }
