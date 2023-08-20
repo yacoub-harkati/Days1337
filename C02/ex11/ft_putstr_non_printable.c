90b309ac1369f60e4895e1f4c33113f66e7b93df
yaharkat <yaharkat@e2r6p2.1337.ma>
2023-08-20 17:27:34 +0100

    C02

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,11 +6,42 @@
 /*   By: yaharkat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 22:44:38 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/08/19 22:45:08 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/08/20 17:24:34 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putstr_non_printable(char *str)
+#include <unistd.h>
+
+void	ft_putchar(char c)
 {
-	
+	write(1, &c, 1);
+}
+
+void	print_hex(unsigned int dec)
+{
+	char	hex[16];
+
+	hex[16] = "0123456789abcdef";
+	ft_putchar('\\');
+	ft_putchar(hex[dec / 16]);
+	ft_putchar(hex[dec % 16]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 32 || str[i] > 127)
+		{
+			print_hex(str[i]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
 }
