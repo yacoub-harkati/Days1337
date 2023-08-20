928ec3988f03ae3b345c6af45a5c3e358cadc3fc
yaharkat <yaharkat@e2r6p2.1337.ma>
2023-08-20 19:09:52 +0100

    hello

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,9 +6,11 @@
 /*   By: yaharkat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 20:02:23 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/08/19 21:05:03 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/08/20 19:08:52 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	is_lower(char c)
 {
@@ -17,6 +19,30 @@ int	is_lower(char c)
 		return (1);
 	}
 	return (0);
+}
+
+int	is_upper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+void to_lower(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (is_upper(str[i]))
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
 }
 
 int	is_num(char c)
@@ -46,7 +72,7 @@ char	to_upper(char c)
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-
+	to_lower(str);
 	i = 1;
 	if (is_alpha(str[0]) && is_lower(str[0]))
 	{
@@ -64,4 +90,11 @@ char	*ft_strcapitalize(char *str)
 		i++;
 	}
 	return (str);
+}
+
+
+int main(void)
+{	
+	char str[] = "tahaHj.";
+	printf("%s ", ft_strcapitalize(str));
 }
