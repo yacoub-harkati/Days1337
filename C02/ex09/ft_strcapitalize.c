4f88ef4927289dda397fec60891738aae23595ee
yaharkat <yaharkat@e2r3p8.1337.ma>
2023-08-21 13:39:36 +0100

    finished c02

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,52 +6,25 @@
 /*   By: yaharkat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 20:02:23 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/08/21 06:58:51 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/08/21 13:36:26 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	is_lower(char c)
+void	to_lower(char *str)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int	is_upper(char c)
-{
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-void to_lower(char *str)
-{
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i])
 	{
-		if (is_upper(str[i]))
+		if ((str[i] >= 'A' && str[i] <= 'Z'))
 		{
 			str[i] += 32;
 		}
 		i++;
 	}
-}
-
-int	is_num(char c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	return (0);
 }
 
 int	is_alpha(char c)
@@ -72,17 +45,18 @@ char	to_upper(char c)
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
+
 	to_lower(str);
 	i = 1;
-	if (is_alpha(str[0]) && is_lower(str[0]))
+	if (is_alpha(str[0]) && (str[0] >= 'a' && str[0] <= 'z'))
 	{
 		str[0] = to_upper(str[0]);
 	}
 	while (str[i])
 	{
-		if (!is_alpha(str[i - 1]) && !is_num(str[i - 1]))
+		if (!is_alpha(str[i - 1]) && !(str[i - 1] >= '0' && str[i - 1] <= '9'))
 		{
-			if (is_lower(str[i]) && is_alpha(str[i]))
+			if ((str[i] >= 'a' && str[i] <= 'z') && is_alpha(str[i]))
 			{
 				str[i] = to_upper(str[i]);
 			}
@@ -90,11 +64,4 @@ char	*ft_strcapitalize(char *str)
 		i++;
 	}
 	return (str);
-}
-
-
-int main(void)
-{	
-	char str[] = "tahaHj.";
-	printf("%s ", ft_strcapitalize(str));
 }
