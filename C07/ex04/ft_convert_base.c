f5f344255f6965f5580408b2849322bad22acf14
Yacoub Harkati <haryacoub@gmail.com>
2023-08-28 16:12:30 +0100

    C07

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaharkat <yaharkat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 15:47:16 by yaharkat          #+#    #+#             */
+/*   Updated: 2023/08/28 16:10:42 by yaharkat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int		check(char *base);
+int		compute_length(int nbr, int base_len);
+int		ft_atoi_base(char *str, char *base);
+
+int	find_str_start(char *str, int *sign)
+{
+	int	index;
+
+	index = 0;
+	while ((str[index] >= 9 && str[index] <= 13) || str[index] == 32)
+	{
+		index++;
+	}
+	while (str[index] == '-' || str[index] == '+')
+	{
+		if (str[index] == '-')
+		{
+			*sign = -*sign;
+		}
+		index++;
+	}
+	return (index);
+}
+
+int	get_pos_in_base(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_convert_base2(int number, int sign, char *base_to, char *tab)
+{
+	int	base_len;
+	int	size;
+	int	i;
+
+	base_len = check(base_to);
+	i = 0;
+	size = compute_length(number, base_len);
+	if (sign)
+	{
+		tab[0] = '-';
+	}
+	if (!tab)
+		return (NULL);
+	while (i < size)
+	{
+		tab[size - i - !sign] = base_to[number % base_len];
+		number /= base_len;
+		i++;
+	}
+	tab[size + sign] = '\0';
+	return (tab);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int		number;
+	int		size;
+	int		base_len;
+	int		sign;
+	char	*final_nbr_in_base;
+
+	sign = 0;
+	size = 0;
+	number = ft_atoi_base(nbr, base_from);
+	base_len = check(base_to);
+	final_nbr_in_base = NULL;
+	if (!base_len)
+		return (NULL);
+	if (number < 0)
+	{
+		sign = 1;
+		number = -number;
+	}
+	size = compute_length(number, base_len);
+	final_nbr_in_base = (char *)malloc((size + 2) * sizeof(char));
+	ft_convert_base2(number, sign, base_to, final_nbr_in_base);
+	return (final_nbr_in_base);
+}
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaharkat <yaharkat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 15:47:18 by yaharkat          #+#    #+#             */
+/*   Updated: 2023/08/28 16:11:29 by yaharkat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	find_str_start(char *str, int *sign);
+int	get_pos_in_base(char c, char *base);
+
+int	is_double_chars(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i])
+	{
+		j = i + 1;
+		while (str[j])
+		{
+			if (str[i] == str[j])
+			{
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	compute_length(int nbr, int base_len)
+{
+	int	count;
+
+	count = 0;
+	while (nbr > 0)
+	{
+		nbr /= base_len;
+		count++;
+	}
+	return (count);
+}
+
+int	check(char *base)
+{
+	int	base_len;
+
+	base_len = 0;
+	while (base[base_len])
+	{
+		if (base[base_len] == '+' || base[base_len] == '-')
+		{
+			return (0);
+		}
+		base_len++;
+	}
+	if (base_len < 2)
+	{
+		return (0);
+	}
+	if (is_double_chars(base))
+	{
+		return (0);
+	}
+	return (base_len);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	long	number;
+	int		baselen;
+	int		sign;
+	int		temp;
+	int		index;
+
+	number = 0;
+	index = 0;
+	sign = 1;
+	baselen = check(base);
+	if (baselen == 0)
+	{
+		return (0);
+	}
+	index = find_str_start(str, &sign);
+	temp = get_pos_in_base(str[index], base);
+	while (str[index] && temp != -1)
+	{
+		number *= (long)baselen;
+		number += (long)temp;
+		index++;
+		temp = get_pos_in_base(str[index], base);
+	}
+	return (number * (long)sign);
+}
