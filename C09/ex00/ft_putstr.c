2dcceb17f8e923e0f1c29bfdf67bab0d38706702
yaharkat <yaharkat@e2r3p9.1337.ma>
2023-09-02 23:35:20 +0100

    made some changes on ex00  c09

--- C09/ex00/ft_putstr.c
+++ C09/ex00/ft_putstr.c
@@ -6,13 +6,13 @@
 /*   By: yaharkat <yaharkat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 23:16:22 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/09/01 22:48:44 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/09/02 23:18:52 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putstrchar(char c)
 {
 	write(1, &c, 1);
 }
@@ -24,7 +24,7 @@ void	ft_putstr(char *str)
 	i = 0;
 	while (str[i])
 	{
-		ft_putchar(str[i]);
+		ft_putstrchar(str[i]);
 		i++;
 	}
 }
