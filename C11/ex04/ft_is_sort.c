6b4c86283a48efebbfcc20216bfcf6935409c25d
yaharkat <yaharkat@e2r4p7.1337.ma>
2023-09-04 20:11:07 +0100

    ex04_rework

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,17 +6,16 @@
 /*   By: yaharkat <yaharkat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 15:28:42 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/09/03 15:40:23 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/09/04 20:08:58 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_sort(int *tab, int length, int (*f)(int, int))
+int	is_asc_order(int *tab, int length, int (*f)(int, int))
 {
 	int	sort_cmp;
 	int	i;
 
 	i = 0;
-	sort_cmp = 0;
 	while (i < length - 1)
 	{
 		sort_cmp = f(tab[i], tab[i + 1]);
@@ -27,4 +26,31 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 		i++;
 	}
 	return (1);
+}
+
+int	is_desc_order(int *tab, int length, int (*f)(int, int))
+{
+	int	sort_cmp;
+	int	i;
+
+	i = 0;
+	while (i < length - 1)
+	{
+		sort_cmp = f(tab[i], tab[i + 1]);
+		if (sort_cmp < 0)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_is_sort(int *tab, int length, int (*f)(int, int))
+{
+	if (is_asc_order(tab, length, f) || is_desc_order(tab, length, f))
+	{
+		return (1);
+	}
+	return (0);
 }
